a25a8fe7df3ac77844324c923960075dea815016
Vishal Singh <vishal.singh@iot83.com>
2022-04-19 12:02:14 +0530

    persisting layout

--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -16,43 +16,55 @@ import DraggablePieChart from '../Components/DraggablePieChart';
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
+let layout=getSavedLayout();
 
 function Dashboard({ dashboardConfig, type }) {
 
     const params = useParams();
 
-    console.log(params)
+   // console.log(params,layout,'layout cnsl')
 
     if (!dashboardConfig) {
         dashboardConfig = [];
     }
     var tableCount = 0;
+
     const getWidgets = (widget, data, action) => {
         switch (widget) {
             case "PieChart":
-                return <div key={widget} data-grid={{ x: 0, y: 0, w: 5, h: 2 }}><PieChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><PieChart data={data} /></div>
             case "HalfCirclePie":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><HalfCirclePie data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><HalfCirclePie data={data} /></div>
             case "VariableRadiusPieChart":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><VariableRadiusPieChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><VariableRadiusPieChart data={data} /></div>
             case "DraggablePieChart":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><DraggablePieChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><DraggablePieChart data={data} /></div>
             case "PieChart3D":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><PieChart3D data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><PieChart3D data={data} /></div>
             case "XYBarChart":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><XYBarChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><XYBarChart data={data} /></div>
             case "SlicedChart":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><SlicedChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><SlicedChart data={data} /></div>
             case "DonutChart":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><DonutChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><DonutChart data={data} /></div>
             case "XYLineChart":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><XYLineChart data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><XYLineChart data={data} /></div>
             case "TableWidget":
                 tableCount++;
-                return <div key={`${widget}-${tableCount}`} data-grid={{ x: 0, y: 1, w: 10, h: 2 }}><TableWidget data={data} action={action} /></div>
+                return <div key={`${widget}-${tableCount}`} data-grid={getLayout(widget)}><TableWidget data={data} action={action} /></div>
             case "CardWidget":
-                return <div key={widget} data-grid={{ x: 0, y: 1, w: 5, h: 2 }}><CardWidget data={data} /></div>
+                return <div key={widget} data-grid={getLayout(widget)}><CardWidget data={data} /></div>
         }
+    }
+
+    const onLayoutChange=(currlayout)=>{
+        localStorage.setItem("layout",JSON.stringify(currlayout));      
+    }
+
+    function getLayout(key){
+        layout=getSavedLayout();
+       let config=layout[key]||{ x: 0, y: 0, w: 5, h: 2 };
+       return config;
     }
 
     return (
@@ -62,7 +74,8 @@ function Dashboard({ dashboardConfig, type }) {
             <ResponsiveReactGridLayout width={1200}
                 breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                 cols={{ lg: 12, md: 12, sm: 12, xs: 12, xxs: 12 }}
-                preventCollision={true}
+                onLayoutChange={onLayoutChange}
+                allowOverlap={false}
             >
                 {
                     cloneDeep(dashboardConfig).map((component) => {
@@ -74,5 +87,15 @@ function Dashboard({ dashboardConfig, type }) {
         </>
     )
 }
+
+function getSavedLayout(){
+    let  tmplayout=JSON.parse(localStorage.getItem("layout"));
+    let obj={};
+    if(tmplayout){tmplayout=tmplayout.map(({w,h,x,y,i})=>{
+        obj= {...obj,[i]:{w,h,x,y}}
+    })}
+    tmplayout=obj;
+       return tmplayout;
+   }
 
 export default Dashboard
