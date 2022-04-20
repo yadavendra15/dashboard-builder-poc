a3b40a4e1bbc1030d4caafb63d8f23cf6aa56756
Yadavendra Yadav <yadavendra.yadav@iot83.com>
2022-04-20 18:04:52 +0530

    generators added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-  
-// );
 const root = createRoot(document.getElementById('root')); 
 root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
